ae34aabeacc83ad820e81c8114fae8d099a72f4c
Libor Zoubek <lzoubek@jezzovo.net>
2016-10-13 16:34:19 +0200

    disable highlighting as it breaks click when filtered at some situations

--- components/select/select.ts
+++ components/select/select.ts
@@ -143,7 +143,7 @@ let styles = `
                (mouseenter)="selectActive(o)"
                (click)="selectMatch(o, $event)">
             <a href="javascript:void(0)" class="dropdown-item">
-              <div [innerHtml]="sanitize(o.text | highlight:inputValue)"></div>
+              <div [innerHtml]="sanitize(o.text)"></div>
             </a>
           </div>
         </li>
@@ -162,7 +162,7 @@ let styles = `
                (click)="selectMatch(o, $event)"
                [ngClass]="{'active': isActive(o)}">
             <a href="javascript:void(0)" class="dropdown-item">
-              <div [innerHtml]="sanitize(o.text | highlight:inputValue)"></div>
+              <div [innerHtml]="sanitize(o.text)"></div>
             </a>
           </div>
         </li>
